48218cec82424ee72ad4d7541998655e3a38a709
alim <Abrekovalim38702@gmail.com>
2024-02-15 04:32:02 +0300

    переписал представление с fastapi на flask

--- src/app/web/routes.py
+++ src/app/web/routes.py
@@ -1,36 +1,51 @@
-import asyncio
+from json import dumps
 from typing import Annotated
+from dataclasses import asdict
 
-from fastapi import APIRouter, Depends as FastAPIDepends, HTTPException
+from flask import Blueprint, Request, Response, jsonify
 
-from dishka.integrations.fastapi import inject, Depends as DishkaDepends
+from dishka.integrations.flask import inject, Depends
 
-from app.database.translation_dao import TranslationDAO
 from app.models.translation import Translation
+from app.models.variation import TranslationVariation
+from app.database.translation_dao import TranslationDAO
 from app.web.schemas import PaginationParams, TranslationParams
 
 
-router = APIRouter(prefix="/translations", tags=["Словарь"])
+blueprint = Blueprint("translations", __name__, url_prefix="/translations")
 
 
-@router.get("/", response_model=list[Translation])
+@blueprint.get("/")
 @inject
-async def get_translations(
-    translation_dao: Annotated[TranslationDAO, DishkaDepends()],
-    pagination_params: Annotated[PaginationParams, FastAPIDepends()],
-    translation_params: Annotated[TranslationParams, FastAPIDepends()],
-) -> list[Translation]:
+def get_translations(
+    request: Annotated[Request, Depends()],
+    translation_dao: Annotated[TranslationDAO, Depends()],
+) -> list[Translation] | tuple[Response, int]:
+    pagination_params = PaginationParams(
+        request.args.get("limit", 20, int),
+        request.args.get("offset", 0, int),
+    )
+    translation_params = TranslationParams(
+        request.args.get("word", "", str),
+        request.args.get(
+            "variation",
+            TranslationVariation.RUSSIAN_CIRCASSIAN.value,
+            str,
+        ),
+    )
     try:
         pagination_params.validate_params()
-
     except ValueError as e:
-        raise HTTPException(400, detail=str(e))
+        return jsonify({"detail": str(e)}), 400
 
     else:
-        return await asyncio.to_thread(
-            translation_dao.get_like,
+        translations = translation_dao.get_like(
             translation_params.variation,
             translation_params.word,
             pagination_params.limit,
             pagination_params.offset,
         )
+        return [
+            dumps(asdict(translation), ensure_ascii=False)
+            for translation in translations
+        ]
